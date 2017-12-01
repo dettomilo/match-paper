17ff4f0b24bc41f84382b21cdc365a8de7d6ad75
dettomilo <edettori7@gmail.com>
2017-12-01 10:11:31 +0100

    Added menu.xml, added search bar, added some boilerplate code

--- app/src/main/java/com/mobile/matchpaper/MainActivity.java
+++ app/src/main/java/com/mobile/matchpaper/MainActivity.java
@@ -1,13 +1,86 @@
 package com.mobile.matchpaper;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    ScrollView svImages;
+    TextView tvResult;
+    URL url;
+    String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        svImages = findViewById(R.id.sv_images);
+        tvResult = findViewById(R.id.tv_result);
+
+        try {
+            url = new URL("https://pixabay.com/api/?key=7232093-5c2e905e26143573763e287dc&q=yellow+flowers&image_type=photo&pretty=true");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            result = getResponseFromHttpUrl(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        tvResult.setText(result);
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if (menuItemThatWasSelected == R.id.action_search){
+            Context context = MainActivity.this;
+            String message = "Search clicked";
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
