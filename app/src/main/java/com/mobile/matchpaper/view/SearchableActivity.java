f6ed0d0dbd3de51b65d15f9e6c0f8279eb4bd052
Emilio Dettori <edettori7@gmail.com>
2017-12-15 14:15:23 +0100

    Forced soft keyboard to open

--- app/src/main/java/com/mobile/matchpaper/view/SearchableActivity.java
+++ app/src/main/java/com/mobile/matchpaper/view/SearchableActivity.java
@@ -1,7 +1,11 @@
 package com.mobile.matchpaper.view;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.SearchView;
 
 import com.mobile.matchpaper.R;
 
@@ -11,5 +15,14 @@ public class SearchableActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
+
+        SearchView searchView = findViewById(R.id.search_view);
+        //searchView.setQuery("Test", true);
+        searchView.setFocusable(true);
+        searchView.setIconified(false);
+        //showSoftKeyboard(searchView);
+        searchView.requestFocus();
+        ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE))
+                .toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
     }
 }
