177a96abbdf56989043c98342a487ad9093f348f
dettomilo <edettori7@gmail.com>
2018-02-07 10:49:02 +0100

    Added button Set Wallpaper. Other fixes here and there

--- app/src/main/java/com/mobile/matchpaper/view/DisplayImageActivity.java
+++ app/src/main/java/com/mobile/matchpaper/view/DisplayImageActivity.java
@@ -3,6 +3,7 @@ package com.mobile.matchpaper.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +12,9 @@ import com.mobile.matchpaper.R;
 public class DisplayImageActivity extends AppCompatActivity {
 
     private ImageView fullScreenImg;
+    private ImageButton favoritesButton;
+    private ImageButton downloadButton;
+    private ImageButton setWallpaperButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,9 @@ public class DisplayImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_image);
 
         fullScreenImg = findViewById(R.id.full_screen_img);
+        favoritesButton = findViewById(R.id.btn_img_favorites);
+        downloadButton = findViewById(R.id.btn_img_download);
+        setWallpaperButton = findViewById(R.id.btn_img_set_wallpaper);
 
         Intent intent = getIntent();
         String imageID = intent.getStringExtra(ListFragment.INTENT_STRING_CONTENT);
