4279e0197ac421b62c56fdb6f1d4c2215e3efbc9
dettomilo <edettori7@gmail.com>
2018-02-07 10:59:29 +0100

    Added the code snippet that actually set the wallpaper

--- app/src/main/java/com/mobile/matchpaper/view/DisplayImageActivity.java
+++ app/src/main/java/com/mobile/matchpaper/view/DisplayImageActivity.java
@@ -1,13 +1,17 @@
 package com.mobile.matchpaper.view;
 
+import android.app.WallpaperManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mobile.matchpaper.R;
+
+import java.io.IOException;
 
 public class DisplayImageActivity extends AppCompatActivity {
 
@@ -30,5 +34,22 @@ public class DisplayImageActivity extends AppCompatActivity {
         String imageID = intent.getStringExtra(ListFragment.INTENT_STRING_CONTENT);
 
         //TODO request image with said ID
+
+
+
+        setWallpaperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WallpaperManager myWallpaperManager = WallpaperManager
+                        .getInstance(getApplicationContext());
+
+                //TODO replace ... with img
+                /*try {
+                    myWallpaperManager.setResource(R.drawable. ... );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+            }
+        });
     }
 }
