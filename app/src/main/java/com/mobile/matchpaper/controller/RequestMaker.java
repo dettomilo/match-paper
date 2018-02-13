b81d16d3a4ab2e1850cef6573c7df35bc3329ed2
Alberto Serluca <alberto.ser11@gmail.com>
2018-02-13 20:35:55 +0100

    Changed result to Latest

--- app/src/main/java/com/mobile/matchpaper/controller/RequestMaker.java
+++ app/src/main/java/com/mobile/matchpaper/controller/RequestMaker.java
@@ -82,7 +82,7 @@ public class RequestMaker {
         String requestURL = buildSearchURL(
                 "",
                 "",
-                ResultsOrder.POPULAR,
+                ResultsOrder.LATEST,
                 pageNumber.toString(),
                 resultsPerPage.toString()
         ).toString();
