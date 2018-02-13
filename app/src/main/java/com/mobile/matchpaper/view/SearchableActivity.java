37e25e3e478b7ba5e96134d98febb89303c24928
Alberto Serluca <alberto.ser11@gmail.com>
2018-02-13 14:53:49 +0100

    Ping Pong Push Little Shit

--- app/src/main/java/com/mobile/matchpaper/view/SearchableActivity.java
+++ app/src/main/java/com/mobile/matchpaper/view/SearchableActivity.java
@@ -1,10 +1,15 @@
 package com.mobile.matchpaper.view;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +26,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mobile.matchpaper.R;
+import com.mobile.matchpaper.controller.ImageVisualizer;
+import com.mobile.matchpaper.controller.RequestMaker;
+import com.mobile.matchpaper.controller.SearchResultReceivedListener;
+import com.mobile.matchpaper.model.ImageContainer;
+import com.mobile.matchpaper.model.JSONSearchResult;
+import com.mobile.matchpaper.model.MatchPaperApp;
+import com.mobile.matchpaper.model.UserPreferences;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SearchableActivity extends AppCompatActivity {
 
@@ -29,10 +45,16 @@ public class SearchableActivity extends AppCompatActivity {
     private TextView textViewSearchResults;
     private GridView gridView;
 
+    private static final String DOWNLOAD_FINISHED_EVENT_NAME = "search_image_download_finished";
+    private static List<ImageContainer> imageContainers = new LinkedList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
+
+        LocalBroadcastManager.getInstance(MatchPaperApp.getContext()).registerReceiver(searchImageDownloadFinished,
+                new IntentFilter(DOWNLOAD_FINISHED_EVENT_NAME));
 
         editTextSearch = findViewById(R.id.et_search);
         imageButtonSearch = findViewById(R.id.ib_search);
@@ -43,10 +65,21 @@ public class SearchableActivity extends AppCompatActivity {
         editTextSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                // TODO Da mettere dentro all'if sotto quando funzionerà? lol boh
+                RequestMaker.searchImagesByQuery(textView.getText().toString(), 1, 24, new SearchResultReceivedListener() {
+                    @Override
+                    public void callListenerEvent(JSONSearchResult results) {
+                        searchResultsReceived(results);
+                    }
+                });
+
                 if (i == EditorInfo.IME_ACTION_DONE) {
                     textViewSearchResults.setVisibility(View.VISIBLE);
                     ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE))
                             .hideSoftInputFromWindow(textView.getWindowToken(), 0);
+
+                    // TODO y u no work?!?
+
                     return true;
                 }
                 return false;
@@ -59,6 +92,36 @@ public class SearchableActivity extends AppCompatActivity {
                 Toast.makeText(SearchableActivity.this, "" + position, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void searchResultsReceived(JSONSearchResult searchResult) {
+        final List<ImageContainer> results = searchResult.getImageList(true);
+        imageContainers.addAll(results);
+
+        // Starts all the downloads for the drawableImages:
+        for (ImageContainer imageToDownload:results){
+            ImageVisualizer.downloadImageAndNotifyView(DOWNLOAD_FINISHED_EVENT_NAME, imageToDownload, ImageVisualizer.ResolutionQuality.PREVIEW);
+        }
+    }
+
+    private BroadcastReceiver searchImageDownloadFinished = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //TODO qua si notifica che l'immagine con id IMAGE ID (Dentro a imageContainers) ha finito il download quindi tocca dire alla view che ora può mostrarla
+
+            // Get extra data included in the Intent
+            String imageID = intent.getStringExtra("loadedImageID");
+            // TODO per prendere l'immagine da mostrare tocca che fai un for per cercare dentro imageContainers quale ha lo stesso ID cercato (mi pare) :)
+            Log.d("SEARCHDOWNLOAD", "Download completed for image: " + imageID);
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // Unregister since the activity is about to be closed.
+        LocalBroadcastManager.getInstance(MatchPaperApp.getContext()).unregisterReceiver(searchImageDownloadFinished);
     }
 
     public class ImageAdapter extends BaseAdapter {
